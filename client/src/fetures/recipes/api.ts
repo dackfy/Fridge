a04ecd2f73bfb80b96952d1052c08c9662ca6eec
Gigorii- <chepurnov2009@yandex.ru>
2023-09-25 18:27:32 +0300

    search

--- client/src/fetures/recipes/api.ts
+++ client/src/fetures/recipes/api.ts
@@ -7,3 +7,19 @@ export const fetchRecipes = async (): Promise<Recipe[]> => {
 
   return res.json();
 };
+// export const initProductsFetch = async (title: string): Promise<Recipe[]> => {
+//   const res = await fetch(`/api/category/${title}`);
+//   const data = await res.json();
+//   return data;
+// };
+// export const initOneProductFetch = async ({
+//   title,
+//   idProd,
+// }: {
+//   title: string;
+//   idProd: string;
+// }): Promise<Recipe> => {
+//   const res = await fetch(`/api/category/${title}/${idProd}`);
+//   const data = await res.json();
+//   return data;
+// };
