f3a7717fccf74f051bb5ec18d2fa40d4e0060aad
vanramon <vono30000@gmail.com>
2023-09-29 01:45:48 +0300

    faled

--- client/src/fetures/404/404.tsx
+++ client/src/fetures/404/404.tsx
@@ -1,47 +1,65 @@
 import React from 'react';
+import './failStyles.css';
 
 function Error(): JSX.Element {
   const enlargedFourStyle = {
-    fontSize: '40rem', 
-    margin: '0 auto', 
+    fontSize: '40rem',
+    margin: '5 auto',
   };
 
   const customFontStyle = {
-    fontSize: '3rem', 
-    fontFamily: 'YourCustomFont', 
+    fontSize: '3rem',
+    fontFamily: 'YourCustomFont',
   };
 
   const redirectToRecipes = () => {
-    
     window.location.href = '/add';
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <div style={customFontStyle}>
-        Такой страницы не существует
+    <>
+      <div className="header" style={customFontStyle}>
+        Такой страницы не существует!
       </div>
+      <div className="containerFail">
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: '160px',
+            height: '170px',
+          }}
+        >
+          <div
+            style={{
+              animation: 'moveLeftRight 2s infinite',
+              ...enlargedFourStyle,
+            }}
+          >
+            4
+          </div>
 
-      <button onClick={redirectToRecipes} style={{ fontSize: '2rem', marginTop: '1rem',borderRadius:'12px',color:'246, 141, 4' }}>
-        Перейти к рецептам
-      </button>
+          <img
+            style={{ width: '450px', animation: 'moveLeftRight 2s infinite' }}
+            src="https://i.gifer.com/origin/fa/fad0e9b11d224f54cf62240fac2851eb_w200.gif"
+            alt="Ошибка"
+          />
 
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '1rem' }}>
-        <div style={{ animation: 'moveLeftRight 2s infinite', ...enlargedFourStyle }}>
-          4
+          <div
+            style={{
+              animation: 'moveLeftRight 2s infinite',
+              ...enlargedFourStyle,
+            }}
+          >
+            4
+          </div>
         </div>
-
-        <img
-          style={{ width: '450px', animation: 'moveLeftRight 2s infinite' }}
-          src="https://i.gifer.com/origin/fa/fad0e9b11d224f54cf62240fac2851eb_w200.gif"
-          alt="Ошибка"
-        />
-
-        <div style={{ animation: 'moveLeftRight 2s infinite', ...enlargedFourStyle }}>
-          4
-        </div>
+        <button className="homeBtn" onClick={redirectToRecipes}>
+          Перейти к рецептам
+        </button>
       </div>
-    </div>
+    </>
   );
 }
 
